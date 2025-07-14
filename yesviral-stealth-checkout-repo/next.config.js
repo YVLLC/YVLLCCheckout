69e6888e7be8a876f260cce9d79c3b2fdb68611f
YVLLC <yesviral@workmail.com>
2025-07-14 14:53:27 -0400

    Delete yesviral-stealth-checkout-repo directory

--- yesviral-stealth-checkout-repo/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Add more config here if needed (images, rewrites, etc)
-};
-
-module.exports = nextConfig;
